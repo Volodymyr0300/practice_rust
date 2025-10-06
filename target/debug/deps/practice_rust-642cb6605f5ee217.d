86ce9ce4c0f21039ce7651bddc57ff4305dda966
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-05 21:16:49 -0500

    initial

---
+++ target/debug/deps/practice_rust-642cb6605f5ee217.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/practice_rust/target/debug/deps/libpractice_rust-642cb6605f5ee217.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/practice_rust/target/debug/deps/practice_rust-642cb6605f5ee217.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/practice_rust-a8734a33b6c13ff7.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/practice_rust/target/debug/deps/libpractice_rust-a8734a33b6c13ff7.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/practice_rust/target/debug/deps/practice_rust-a8734a33b6c13ff7.d: src/main.rs
+
+src/main.rs:
