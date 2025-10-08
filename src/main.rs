257d7609ea1bcb93b52d8a7cb7b852c0784ce7f8
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-07 21:09:32 -0500

    some practice

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn find_short(s: &str) -> u32 { // "bitcoin take over the world maybe who knows 
         .unwrap_or(0) // Return 0 if there are no words
 }
 
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 fn quarter_of(month: u8) -> u8 {
     /*
     if  month <= 3 {
@@ -32,7 +33,39 @@ fn quarter_of(month: u8) -> u8 {
     (month - 1) / 3 + 1
 }
 
+// function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+fn between(a: i16, b: i16) -> Vec<i16> {
+    (a..=b).collect()
+}
+
+// When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9. (For example: 0 "zero", 1 "one", 2 "two", etc.) If your language supports it, try using a switch statement.
+fn switch_it_up(n: usize) -> &'static str {
+    match n {
+        0 => "zero",
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        4 => "four",
+        5 => "five",
+        6 => "six",
+        7 => "seven",
+        8 => "eight",
+        9 => "nine",
+        _ => panic!("Input must be between 0 and 9"),
+    }
+}
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+fn descending_order(x: u64) -> u64 {
+    let mut digits: Vec<char> = x.to_string().chars().collect(); // Convert the number to a string and then to a vector of characters
+    digits.sort_by(|a, b| b.cmp(a)); // Sort the characters in descending order
+    digits.iter().collect::<String>().parse::<u64>().unwrap() // Collect the characters back into a string and parse it as u64
+}
+
 fn main() {
     println!("The shortest word contains {} characters.", find_short("bitcoin take over the world maybe who knows perhaps"));
-    println!("The Quarter of Month is {}", quarter_of(4))
+    println!("The Quarter of Month is {}", quarter_of(4));
+    println!("The array of all integers between the input parameters is {:?}", between(1, 5));
+    println!("The number in words is {}", switch_it_up(7));
+    println!("The highest possible number is {}", descending_order(872345));
 }
